27f31362bbb9182156bc069fbfb9b9715175a33f
Mikigb77 <miquelgarcia.alu@gmail.com>
2024-03-10 15:50:37 +0100

    Update for my User model

--- ppf/common/models/user.py
+++ ppf/common/models/user.py
@@ -1,12 +1,17 @@
 """
-    This is the file containing all the classes and models we are gonna use
+Here is the common models for all the applications.
+
+This will be shared through all the dockers and can be accesses by importing it.
+
+__example: from ppf.common.models import User, Driver
 """
 
 from django.contrib.auth.models import User as baseUser
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class MyUser(baseUser):
+class User(baseUser):
     """
     User class for our project
 
@@ -16,10 +21,18 @@ class MyUser(baseUser):
     """
     birth_date = models.DateField(auto_now=False, auto_now_add=False)
     points = models.IntegerField()
+    updated_at = models.DateTimeField(
+        "Last modification of the User", auto_now=True, auto_now_add=False)
+    created_at = models.DateTimeField(
+        "Creation date of the User", auto_now=False, auto_now_add=True)
+
     # profile_image = models.ImageField(
     #   upload_to=None, height_field=None, width_field=None, max_length=None)
 
     class Meta:
+        """
+            Meta used to add the label so that the imports work correctly
+        """
         app_label = 'common'
 
 
@@ -32,4 +45,35 @@ class Driver(baseUser):
     driver_points = models.IntegerField()
 
     class Meta:
+        """
+            Meta used to add the label so that the imports work correctly
+        """
+        app_label = 'common'
+
+
+class Valuation(models.Model):
+    """
+    Model for storing valuations given by users to users or drivers.
+    """
+    RATING_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+
+    giver = models.ForeignKey(
+        User, related_name='given_valuations', on_delete=models.CASCADE)
+    receiver_user = models.ForeignKey(
+        User, related_name='received_user_valuations',
+        on_delete=models.CASCADE, null=True, blank=True)
+    rating = models.IntegerField(choices=RATING_CHOICES, validators=[
+                                 MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField(blank=True)
+
+    class Meta:
+        """
+            Meta used to add the label so that the imports work correctly
+        """
         app_label = 'common'
