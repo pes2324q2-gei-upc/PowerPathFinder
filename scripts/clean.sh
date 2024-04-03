d19c24d622bac4c90c6f2e199fe03f002e833e8b
pg-goose <pau.galopa.dev@gmail.com>
2024-04-04 00:29:21 +0200

    First commit, added configs

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,4 @@
-sudo echo "Started project cleanup"
+sudo "Started project cleanup"
 echo "Composing down containers, this might take a while"
 docker compose -f 'docker-compose.development.yml' down > /dev/null 2>&1
 
@@ -6,6 +6,7 @@ if [ "$1" = "ultra" ]; then
     echo "Prunning all docker system, this may take a while"
     sudo docker system prune -af > /dev/null 2>&1 &
 fi
+
 
 echo "Removing all python cache files, migrations, database, build files"
 sudo find . -type d -name '__pycache__' -exec rm -fr {} + > /dev/null 2>&1 &
@@ -21,12 +22,3 @@ sudo .venv/bin/pip uninstall -y ppf > /dev/null 2>&1 &
 wait
 echo "🧼 Clean complete 🧼"
 echo ""
-
-
-
-
-
-
-
-
-
