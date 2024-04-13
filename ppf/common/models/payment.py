f636097f31a92cb2b6081cfb281d99a1cf14ca17
LightBlue247 <zongjun1031@gmail.com>
2024-04-13 15:16:25 +0200

    1. Added en settings.json la ruta para acceder a ppf-payments-api porque se necesita una funcion del repo. 2. Creado los modelos para mantener un historial de pagos y reembolsos.

---
+++ ppf/common/models/payment.py
@@ -0,0 +1,28 @@
+from django.db import models
+from rsa import encrypt
+
+from .user import User
+from .route import Route
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=100)
+    paymentIntentId = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user.username}'s payment of {self.amount} on {self.date}"
+
+
+class Refund(models.Model):
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    reason = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Refund of {self.amount} for payment {self.payment} on {self.date}"
