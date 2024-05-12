45a9d01b805cdcc132fdb0a9ec32456408934e56
Pecmecy <eldomi99@hotmail.es>
2024-05-13 00:27:00 +0200

    [#170] Modify user model to add login type

--- ppf/common/models/user.py
+++ ppf/common/models/user.py
@@ -18,6 +18,15 @@ class User(baseUser):
         baseUser (User): default django implementation for user
             (see the documentation for more info)
     """
+    Google = "google"
+    Facebook = "facebook"
+    Base = "base"
+
+    typeLoginChoices = [
+        (Google, "google"),
+        (Facebook, "facebook"),
+        (Base, "base")
+    ]
 
     # change to keep the pk defined in the UML consistent
     baseUser.email = models.EmailField(
@@ -32,6 +41,9 @@ class User(baseUser):
 
     profileImage = models.ImageField(
         upload_to="profile_image", null=True, blank=True, default="default.png")
+
+    typeOfLogin = models.CharField(
+        max_length=50, choices=typeLoginChoices, default=Base)
 
     class Meta:
         app_label = "common"
@@ -160,3 +172,20 @@ class Report(models.Model):
 
     class Meta:
         app_label = "common"
+
+
+class TypeOfLogin(models.Model):
+    """
+    Model to represent the types of logins.
+    """
+    Google = "google"
+    Facebook = "facebook"
+    Base = "base"
+
+    typeChoices = [
+        (Google, "google"),
+        (Facebook, "facebook"),
+        (Base, "base")
+    ]
+
+    typeOfLogin = models.CharField(max_length=50, choices=typeChoices)
