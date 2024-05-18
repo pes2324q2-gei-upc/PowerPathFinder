950b3818e1dc73885ca894d8509e96efb8c94ad1
pg-goose <pau.galopa.dev@gmail.com>
2024-05-18 15:50:39 +0200

    added more filed fields

--- ppf/common/models/fcm.py
+++ ppf/common/models/fcm.py
@@ -7,7 +7,7 @@ class FCMToken(models.Model):
     """
 
     user = models.OneToOneField("User", on_delete=models.CASCADE, related_name="fcm_token")
-    token = models.CharField(unique=True)
+    token = models.CharField(unique=True, max_length=255, null=True)
     ts = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
